75d94efd53b099ee2db1ebe7f3b7bc4c493f73b4
Royyan Dzakiy <r.dzakiy@lmesh.eu>
2025-07-19 10:16:07 +0700

    succesfully running imu module

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
-#include "display.h"
-// #include "imu.h"
+// #include "display.h"
+#include "imu.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_LOG_DEFAULT_LEVEL);
@@ -8,14 +8,14 @@ int main(void)
 {
     LOG_INF("Main Start");
 
-	// imu_init();
-    display_init();
+	imu_init();
+    // display_init();
 
     k_msleep(1000);
 
     while(1) {
-        // imu_loop();
-        display_loop();
+        imu_loop();
+        // display_loop();
     }
  
     LOG_INF("Main End");
